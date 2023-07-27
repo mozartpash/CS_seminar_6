4d8ea5f72d6ae1772266929825c20c484f070752
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-27 19:37:14 +0300

    added tasks

--- task040/Program.cs
+++ task040/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 
+int GetNumber(string abc)
+{
+    Console.Write($"{abc} ");
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void Triangle(int a, int b, int c)
+{
+    if (a + b >= c || a + c >= b|| b + c >= a)
+Console.Write("Треугольник может существовать");
+else
+    Console.Write("Треугольник не может существовать");
+}
+
+int a = GetNumber("Введите сторону a:");
+int b = GetNumber("Введите сторону b:");
+int c = GetNumber("Введите сторону c:");
+
+Triangle(a, b, c);
---
+++ task041/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// string res1=Convert.ToString(number,2); - переводит в двоичное число
+//-----------------------------------------Один вариант с записью в строку-----------------------------------
+// string result = "";
+// while (number > 0)
+// {
+//     result = (number % 2) + result;
+//     number = number / 2;
+// }
+// Console.Write(int.Parse(result));
+//-----------------------------------------------------------------------------------------------------------
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int CountDiv(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 2;
+        count++;
+    }
+    return count;
+}
+
+void BinaryNumber(int num)
+{
+    int b = 0;
+    int i = 0;
+    while (a > 0)
+    {
+        b = a % 2;
+        a = a / 2;
+        binary[binary.Length - 1 - i] = b;
+        i++;
+    }
+    Console.Write(String.Join("", binary));
+}
+
+int a = GetNumber("Введите число: ");
+
+int countDivide = CountDiv(a);
+
+int[] binary = new int[countDivide];
+
+BinaryNumber(a);
+
+
+// --------------------------------------------------------------
+
+//Надо доделать
